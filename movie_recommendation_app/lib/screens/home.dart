6c938e931301e265adfca64f76dddb57815af861
Kacper Straszak <k3acper@o2.pl>
2025-11-19 00:42:04 +0100

    movies showing up on home page

---
+++ movie_recommendation_app/lib/screens/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_recommendation_app/providers/movie_provider.dart';
+import 'package:movie_recommendation_app/utils/constants.dart';
+import 'package:movie_recommendation_app/widgets/menu_drawer.dart';
+import 'package:movie_recommendation_app/widgets/movie_element.dart';
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final movies = ref.watch(moviesProvider);
+
+    Widget content = preloader;
+    if (movies.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (ctx, index) {
+          return MovieElement(movie: movies[index]);
+        },
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      appBar: AppBar(
+        title: const Text('Movie Recommendation App'),
+      ),
+      drawer: const MenuDrawer(),
+      body: content,
+    );
+  }
+}
