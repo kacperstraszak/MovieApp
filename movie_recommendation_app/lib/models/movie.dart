b290c8a46f34e4f967b669335b16d4504b220b0a
Kacper Straszak <k3acper@o2.pl>
2025-12-03 10:58:28 +0100

    changed movie data table

--- movie_recommendation_app/lib/models/movie.dart
+++ movie_recommendation_app/lib/models/movie.dart
@@ -1,14 +1,29 @@
 class Movie {
+  const Movie({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.posterPath,
+    this.backdropPath,
+    required this.releaseDate,
+  });
+
   final int id;
   final String title;
   final String description;
   final String posterPath;
+  final String? backdropPath;
   final String releaseDate;
 
-  Movie.fromJson(Map<String, dynamic> json)
-      : id = json['id'] as int,
-        title = json['title'] as String,
-        description = json['description'] as String,
-        posterPath = 'https://image.tmdb.org/t/p/w500${json['poster_path']}',
-        releaseDate = json['release'] as String;
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      description: json['description'] as String? ?? '',
+      posterPath: json['poster_path'] as String? ?? '',
+      backdropPath: json['backdrop_path'] as String?, 
+      releaseDate: json['release'] as String? ?? 'Unknown',
+    );
+  }
 }
