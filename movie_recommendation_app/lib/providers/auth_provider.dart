455ee1711a3bd71a4f94e43db248cba2c8edd3c8
Kacper Straszak <k3acper@o2.pl>
2025-12-11 19:26:43 +0100

    authstate -> myauthstate

--- movie_recommendation_app/lib/providers/auth_provider.dart
+++ movie_recommendation_app/lib/providers/auth_provider.dart
@@ -1,54 +1,24 @@
 import 'dart:io';
 import 'dart:math';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_recommendation_app/models/my_auth_state.dart';
 import 'package:movie_recommendation_app/models/user_profile.dart';
 import 'package:movie_recommendation_app/utils/constants.dart';
 import 'package:movie_recommendation_app/utils/storage_service.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-// Stan providera
-class AuthState {
-  const AuthState({
-    this.user,
-    this.profile,
-    this.isAuthenticating = false,
-    this.errorMessage,
-  });
-
-  final User? user; // auth zalogowanego użytkownika (mail/id)
-  final UserProfile?
-      profile; // reszta elementów profilu użytkownika (imageUrl/username)
-  final bool
-      isAuthenticating; // stan - czy apliakcja przeprowadza teraz autentykacje z supabasem
-  final String? errorMessage;
-
-  AuthState copyWith({
-    User? user,
-    UserProfile? profile,
-    bool? isAuthenticating,
-    String? errorMessage,
-  }) {
-    return AuthState(
-      user: user ?? this.user,
-      isAuthenticating: isAuthenticating ?? this.isAuthenticating,
-      errorMessage: errorMessage, // Jeśli null, to błąd znika
-      profile: profile ?? this.profile,
-    );
-  }
-}
-
-class AuthNotifier extends Notifier<AuthState> {
+class AuthNotifier extends Notifier<MyAuthState> {
   final _storageService = StorageService();
 
   @override
-  AuthState build() {
+  MyAuthState build() {
     final currentUser = supabase.auth.currentUser;
 
     if (currentUser != null) {
       Future.microtask(() => _loadProfile(currentUser.id));
     }
 
-    return AuthState(user: currentUser);
+    return MyAuthState(user: currentUser);
   }
 
   Future<void> _loadProfile(String userId) async {
@@ -167,9 +137,9 @@ class AuthNotifier extends Notifier<AuthState> {
 
   Future<void> signOut() async {
     await supabase.auth.signOut();
-    state = const AuthState(user: null);
+    state = const MyAuthState(user: null);
   }
 }
 
 final authProvider =
-    NotifierProvider<AuthNotifier, AuthState>(AuthNotifier.new);
+    NotifierProvider<AuthNotifier, MyAuthState>(AuthNotifier.new);
